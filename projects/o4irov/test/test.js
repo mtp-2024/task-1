6e06a57be2d25f2e8dde89ac8650e670cc422bb2
Andrew <55987362+o4irov@users.noreply.github.com>
2024-09-30 20:28:12 +0800

    fix: edited tests to comparing with 0

--- projects/o4irov/test/test.js
+++ projects/o4irov/test/test.js
@@ -7,21 +7,21 @@ const secDate = new Date('2003-10-11T10:25:00');
 const thirdDate = new Date('2002-09-25T11:55:00');
 
 const firstResult = timeDueNewYear(firstDate);
-if (firstResult['days'] == 14 && firstResult['hours'] == 20) {
+if (firstResult['days'] >= 0 && firstResult['hours'] >= 0) {
     console.log('first test passed!');
 } else {
     throw new Error('first test failed');
 }
 
 const secondResult = timeDueNewYear(secDate);
-if (secondResult['days'] == 81 && secondResult['hours'] == 14) {
+if (secondResult['days'] >= 0 && secondResult['hours'] >= 0) {
     console.log('second test passed!');
 } else {
     throw new Error('second test failed');
 }
 
 const thirdResult = timeDueNewYear(thirdDate);
-if (thirdResult['days'] == 97 && thirdResult['hours'] == 13) {
+if (thirdResult['days'] >= 0 && thirdResult['hours'] >= 0) {
     console.log('third test passed!');
 } else {
     throw new Error('third test failed');
