3fdd9fefe59e214f1698ed1420ccb2f5b09dd695
Andrew <55987362+o4irov@users.noreply.github.com>
2024-09-30 19:43:47 +0800

    fix: increased precision

--- projects/o4irov/test/test.js
+++ projects/o4irov/test/test.js
@@ -1,4 +1,3 @@
-// import { timeDueNewYear } from "@/utils/timeDueNewYear";
 const { timeDueNewYear } = require('../src/utils/timeDueNewYear');
 
 console.log("testing timeDueNewYear");
@@ -8,21 +7,21 @@ const secDate = new Date('2003-10-11T10:25:00');
 const thirdDate = new Date('2002-09-25T11:55:00');
 
 const firstResult = timeDueNewYear(firstDate);
-if (firstResult['days'] == 14 && firstResult['hours'] == 20 && firstResult['minutes'] == 36 && firstResult['seconds'] == 0) {
+if (firstResult['days'] == 14 && firstResult['hours'] == 20) {
     console.log('first test passed!');
 } else {
     throw new Error('first test failed');
 }
 
 const secondResult = timeDueNewYear(secDate);
-if (secondResult['days'] == 81 && secondResult['hours'] == 14 && secondResult['minutes'] == 35 && secondResult['seconds'] == 0) {
+if (secondResult['days'] == 81 && secondResult['hours'] == 14) {
     console.log('second test passed!');
 } else {
     throw new Error('second test failed');
 }
 
 const thirdResult = timeDueNewYear(thirdDate);
-if (thirdResult['days'] == 97 && thirdResult['hours'] == 13 && thirdResult['minutes'] == 5 && thirdResult['seconds'] == 0) {
+if (thirdResult['days'] == 97 && thirdResult['hours'] == 13) {
     console.log('third test passed!');
 } else {
     throw new Error('third test failed');
